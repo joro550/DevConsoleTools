073b77b0cede481ac5e4a041d0a8b7df53ccb0ea
mdavies <mdavies2@webroot.com>
2022-05-30 13:08:22 +0100

    Ipsum generator

---
+++ CommandLine/Generators/LoremIpsumCommand.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+using Spectre.Cli;
+using Spectre.Console;
+
+namespace CommandLine.Generators;
+
+public class LoremIpsumCommand : Command<LoremIpsumCommand.Settings>
+{
+    public class Settings : CommandSettings
+    {
+        [DefaultValue(3), 
+         CommandOption("--word-count")]
+        public int WordCount { get; set; }
+    }
+
+    public override int Execute(CommandContext context, Settings settings)
+    {
+        var enumerable = string.Join(" ", Faker.Lorem.Words(settings.WordCount));
+        AnsiConsole.WriteLine(enumerable);
+        return 1;
+    }
+}
--- CommandLine/Program.cs
+++ CommandLine/Program.cs
@@ -7,41 +7,52 @@ var app = new CommandApp();
 
 app.Configure(config =>
 {
+    config.ValidateExamples();
+    
+    config.AddCommand<GenerateGuidCommand>("gg")
+        .WithDescription("generate random guid")
+        .WithExample(new[] {"gg", "--hyphens", "true", "--uppercase", "true"})
+        .WithExample(new[] {"gg", "-h", "true", "-u", "true"});
+    
     config.AddBranch("generate", set =>
     {
         set.AddCommand<GenerateGuidCommand>("guid")
             .WithDescription("generate random guid")
-            .WithExample(args);
+            .WithExample(new[] {"generate", "guid", "--hyphens", "true", "--uppercase", "true"});
         
         set.AddCommand<GenerateEmailCommand>("email")
-            .WithDescription("generate random guid")
-            .WithExample(args);
+            .WithDescription("generate random email")
+            .WithExample(new[] {"generate", "email"});
         
         set.AddCommand<RandomNumberCommand>("number")
             .WithDescription("generate random number")
-            .WithExample(args);
+            .WithExample(new[] {"generate", "number", "--max", "100", "--min", "50"});
+        
+        set.AddCommand<LoremIpsumCommand>("lorem")
+            .WithDescription("generate lorem ipsum")
+            .WithExample(new[] {"generate", "lorem"});
     });
 
     config.AddBranch("base64", set =>
     {
         set.AddCommand<Base64Encode>("encode")
             .WithDescription("base64 encode string")
-            .WithExample(args);
+            .WithExample(new[] {"base64", "encode", "value"});
 
         set.AddCommand<Base64Decode>("decode")
             .WithDescription("base64 decode string")
-            .WithExample(args);
+            .WithExample(new[] {"base64", "decode", "value"});
     });
     
     config.AddBranch("string", set =>
     {
         set.AddCommand<StringLowerCommand>("to-lower")
             .WithDescription("lower cases string")
-            .WithExample(args);
+            .WithExample(new[] {"string", "to-lower", "value"});
         
         set.AddCommand<StringUpperCommand>("to-upper")
             .WithDescription("upper cases string")
-            .WithExample(args);
+            .WithExample(new[] {"string", "to-upper", "value"});
     });
 });
 
